c282df5298880a1db592aaaf9dbb16721d11da1f
Evelyn Chen <evelync93@gmail.com>
2020-06-17 12:33:35 -0400

    starter files

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+const ACCESS_TOKEN =
+  "pk.eyJ1IjoiZXZjaGVuIiwiYSI6ImNrYmppMG9pZzBsc2UycnBkZ2Vzanl3aWkifQ.fNIsmkaJ55MEPkEz-rGShg";
+const MAPBOX_STYLE = "mapbox://styles/evchen/ckbjkg8x10m8i1itfugliru9p/draft";
+
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+var map = new mapboxgl.Map({
+  container: "map",
+
+  style: MAPBOX_STYLE,
+  center: [-77.04, 38.907],
+  zoom: 14,
+});
