5433907c1a98e215109afd0100af89b16164467d
Evelyn Chen <evelync93@gmail.com>
2020-06-18 12:18:56 -0400

    add points of interest

--- dist/script.js
+++ dist/script.js
@@ -7,8 +7,8 @@ mapboxgl.accessToken = ACCESS_TOKEN;
 var map = new mapboxgl.Map({
   container: "map",
   style: MAPBOX_STYLE,
-  center: [-74.005974, 40.712776],
-  zoom: 8,
+  center: [-74.005974, 40.73061],
+  zoom: 13,
 });
 
 //search function
@@ -36,3 +36,104 @@ map.addControl(
   }),
   "top-left"
 );
+
+//popup on hover
+map.on("load", function () {
+  map.addSource("places", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {
+            description:
+              "<strong>Raku</strong><p>48 MacDougal St, New York, NY 10012</p><p>Japanese handmade udon</p>",
+            icon: "restaurant-noodle",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [-74.00252, 40.72724],
+          },
+        },
+        {
+          type: "Feature",
+          properties: {
+            description:
+              "<strong>Prince Street Pizza</strong><p>27 Prince St New York, NY 10012</p><p>Best Sicilian Pepperoni Slices</p>",
+            icon: "restaurant-pizza",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [-74.00252, 40.73724],
+          },
+        },
+        {
+          type: "Feature",
+          properties: {
+            description:
+              "<strong>to updated</strong><p>27 Prince St New York, NY 10012</p><p>Best Sicilian Pepperoni Slices</p>",
+            icon: "bar",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [-74.00252, 40.73724],
+          },
+        },
+        {
+          type: "Feature",
+          properties: {
+            description:
+              "<strong>to updated</strong><p>27 Prince St New York, NY 10012</p><p>Best Sicilian Pepperoni Slices</p>",
+            icon: "restaurant",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [-74.00252, 40.73724],
+          },
+        },
+      ],
+    },
+  });
+
+  // Add a layer showing the places.
+  map.addLayer({
+    id: "places",
+    type: "symbol",
+    source: "places",
+    layout: {
+      "icon-image": "{icon}-15",
+      "icon-allow-overlap": true,
+    },
+  });
+
+  // Create a popup, but don't add it to the map yet.
+  var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+  });
+
+  map.on("mouseenter", "places", function (e) {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = "pointer";
+
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var description = e.features[0].properties.description;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates).setHTML(description).addTo(map);
+  });
+
+  map.on("mouseleave", "places", function () {
+    map.getCanvas().style.cursor = "";
+    popup.remove();
+  });
+});
